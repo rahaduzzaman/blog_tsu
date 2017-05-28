8dbe9b3669c02eb838e0b136c57f743443345f2e
rahaduzzaman <rahad1@ymail.com>
2017-05-29 00:50:12 +0600

    ready for wordpress conversion with scss update

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,48 @@
-var gulp      = require('gulp'),
-    uglify    = require('gulp-uglify'),
-    uglifycss = require('gulp-uglifycss');
+'use strict';
 
+var gulp 			= require('gulp'),
+	browserSync 	= require('browser-sync').create(),
+	sass 			= require('gulp-sass'),
+	uglify 			= require('gulp-uglify'),
+	plumber 		= require('gulp-plumber'),
+	watch 			= require('gulp-watch');
 
-//uglify js scripts
-gulp.task('js', function(){
-  gulp.src('js/bootstrap.js')
-      .pipe(uglify())
-      .pipe(gulp.dest('dest'))
+gulp.task('sass', function() {
+	return gulp.src('./scss/*.scss')
+  .pipe(sass({
+	  outputStyle: 'expanded'
+  }).on('error', sass.logError))
+  .pipe(gulp.dest('./'))
+  .pipe(browserSync.stream());
 });
 
-//uglify style sheets
-gulp.task('css', function(){
-  gulp.src('./css/home.css')
-  .pipe(uglifycss())
-  .pipe(gulp.dest('./dest'));
+// js uglify/minify
+gulp.task('js', function () {
+	return gulp.src('js/*js')
+	.pipe(plumber())
+	.pipe(gulp.dest('./js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('min/js'));
 });
 
-//Watch file changes
-gulp.task('watch',function(){
-  gulp.watch('./css/home.css',['css']);
+// reloading browsers
+gulp.task('js-watch', ['js'], function (done) {
+	browserSync.reload();
+	done();
 });
 
-//default
-//gulp.task('default',['js','css']);
+// Static Server + watching scss/js/html files
+gulp.task('serve', ['sass'], function() {
+
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+
+	gulp.watch("./scss/*.scss", ['sass']);
+	gulp.watch("js/*.js", ['js-watch']);
+	gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
